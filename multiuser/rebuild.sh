ea8c02a5160fc47272d960eb5de220a9cfbf0f24
zidan <daniel.zinkiewicz@wasat.pl>
2018-08-30 10:36:33 +0200

    Add own Geoserver build

--- multiuser/rebuild.sh
+++ multiuser/rebuild.sh
@@ -2,12 +2,13 @@
 
 docker-compose down
 # docker rmi -f jupytepide-hub
-docker rmi -f jupytepide-hub-user
+docker rmi -f jupytepide-hub-user:latest
 
-# make build
+#make build
 make jupytep_user_spawn_image
 
 cd /opt
+rm -rf /opt/JupyTEPIDE/
 git clone -b dev --single-branch https://github.com/wasat/JupyTEPIDE.git
 cd ~/jupytep-dev/multiuser
 
