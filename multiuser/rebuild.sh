8939abe4ff6bb22a4dbd9763afd43b3f7812c062
zidan <daniel.zinkiewicz@wasat.pl>
2018-08-31 12:11:02 +0200

    Add R components - rgdal - failed

--- multiuser/rebuild.sh
+++ multiuser/rebuild.sh
@@ -2,15 +2,15 @@
 
 docker-compose down
 # docker rmi -f jupytepide-hub
-docker rmi -f jupytepide-hub-user:latest
+# docker rmi -f jupytepide-hub-user:latest
 
 #make build
 make jupytep_user_spawn_image
 
-cd /opt
-rm -rf /opt/JupyTEPIDE/
-git clone -b dev --single-branch https://github.com/wasat/JupyTEPIDE.git
-cd ~/jupytep-dev/multiuser
-
+#cd /opt
+#rm -rf /opt/JupyTEPIDE/
+#git clone -b dev --single-branch https://github.com/wasat/JupyTEPIDE.git
+#cd ~/jupytep-dev/multiuser
+#
 docker-compose up -d
 docker ps -a
