febdc75818c3ec744ea4ce8ffea17950b0ee7bd8
Daniel Zinkiewicz <zinkiewicz.daniel@gmail.com>
2018-09-01 14:56:27 +0200

    Repair Pixiedust debugger - add libs to dockerfile

--- multiuser/rebuild.sh
+++ multiuser/rebuild.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker-compose down
+#docker-compose down
 # docker rmi -f jupytepide-hub
 docker rmi -f jupytepide-hub-user:latest
 
@@ -10,8 +10,7 @@ make jupytep_user_spawn_image
 cd /opt
 rm -rf /opt/JupyTEPIDE/
 git clone -b dev --single-branch https://github.com/wasat/JupyTEPIDE.git
-cp -rf /opt/JupyTEPIDE/GUI/res/pixiedust/* "/opt/conda/lib/python3.6/site-packages/pixiedust"
 cd ~/jupytep-dev/multiuser
 
-docker-compose up -d
+# docker-compose up -d
 docker ps -a
