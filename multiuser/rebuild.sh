4f0d98fea2f0594f63e8d8b3c258a217bbf76f61
Daniel Zinkiewicz <zinkiewicz.daniel@gmail.com>
2018-09-01 11:13:13 +0200

    Repair Pixiedust debugger

--- multiuser/rebuild.sh
+++ multiuser/rebuild.sh
@@ -2,15 +2,16 @@
 
 docker-compose down
 # docker rmi -f jupytepide-hub
-# docker rmi -f jupytepide-hub-user:latest
+docker rmi -f jupytepide-hub-user:latest
 
-#make build
-# make jupytep_user_spawn_image
+# make build
+make jupytep_user_spawn_image
 
-#cd /opt
-#rm -rf /opt/JupyTEPIDE/
-#git clone -b dev --single-branch https://github.com/wasat/JupyTEPIDE.git
-#cd ~/jupytep-dev/multiuser
+cd /opt
+rm -rf /opt/JupyTEPIDE/
+git clone -b dev --single-branch https://github.com/wasat/JupyTEPIDE.git
+cp -rf /opt/JupyTEPIDE/GUI/res/pixiedust/* "/opt/conda/lib/python3.6/site-packages/pixiedust"
+cd ~/jupytep-dev/multiuser
 
 docker-compose up -d
 docker ps -a
