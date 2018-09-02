a14d15e8dc9018f1e96bb2f5344bee181ca059f4
zidan <daniel.zinkiewicz@wasat.pl>
2018-09-02 14:39:35 +0200

    rpy2 added

--- multiuser/rebuild.sh
+++ multiuser/rebuild.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-docker-compose down
+# docker-compose down
 # docker rmi -f jupytepide-hub
 # docker rmi -f jupytepide-hub-user:latest
 
 # make build
-# make jupytep_user_spawn_image
+make jupytep_user_spawn_image
 
-#cd /opt
-#rm -rf /opt/JupyTEPIDE/
-#git clone -b dev --single-branch https://github.com/wasat/JupyTEPIDE.git
-#cd ~/jupytep-dev/multiuser
+cd /opt
+rm -rf /opt/JupyTEPIDE/
+git clone -b dev --single-branch https://github.com/wasat/JupyTEPIDE.git
+cd ~/jupytep-dev/multiuser
 
-docker-compose up -d
+# docker-compose up -d
 docker ps -a
