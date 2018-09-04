84a2569082b07e6680c9f0b5829a82a72018984f
zidan <daniel.zinkiewicz@wasat.pl>
2018-09-04 16:32:17 +0200

    WMS - add layer name

--- sdk/jupytep/maps/content.py
+++ sdk/jupytep/maps/content.py
@@ -5,7 +5,8 @@ from jupytep.maps.wms.parse import get_wms_base_url, get_layer_name
 def show_on_map(geotiff_path):
     wmsL = WMSLayer()
     wms_url = get_wms_base_url()
-    layer = 'demo:' + get_layer_name(geotiff_path)
+    layer_name = get_layer_name(geotiff_path)
+    layer = 'demo:' + layer_name
     parameters = {'layers': layer, 'format': 'image/png', 'transparent': 'true'}
-    wmsL.add_wms_layer(wms_url, 'moje', parameters)
+    wmsL.add_wms_layer(wms_url, layer_name, parameters)
     wmsL.show_layer()
