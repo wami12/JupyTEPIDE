9612f47d0aeea52205c8f625f4873b59d1f685f5
zidan <daniel.zinkiewicz@wasat.pl>
2018-09-17 13:01:22 +0200

    bugfixing

--- multiuser/rebuild.sh
+++ multiuser/rebuild.sh
@@ -7,14 +7,14 @@ docker rmi -f jupytepide-hub-user:latest
 # make build
 make jupytep_user_spawn_image
 
-#cd /opt
-#rm -rf /opt/JupyTEPIDE/
-#git clone -b dev --single-branch https://github.com/wasat/JupyTEPIDE.git
-#rm -rf /opt/JupyTEPIDE/sdk/
-#ln -s ~/jupytep-dev/sdk/ /opt/JupyTEPIDE
-#ln -s /eodata /eodata-demo
-#ln -s /opt/eodata /eodata-opt
-#cd ~/jupytep-dev/multiuser
+cd /opt
+rm -rf /opt/JupyTEPIDE/
+git clone -b dev --single-branch https://github.com/wasat/JupyTEPIDE.git
+rm -rf /opt/JupyTEPIDE/sdk/
+ln -s ~/jupytep-dev/sdk/ /opt/JupyTEPIDE
+ln -s /eodata /eodata-demo
+ln -s /opt/eodata /eodata-opt
+cd ~/jupytep-dev/multiuser
 
 docker-compose up -d
 docker ps -a
