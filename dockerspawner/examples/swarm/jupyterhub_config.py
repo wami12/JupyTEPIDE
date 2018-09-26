f667b121980ac9e018fd7b7b68cefced36a97ec9
Daniel Zinkiewicz <zinkiewicz.daniel@gmail.com>
2018-09-27 01:04:03 +0200

    SwarmSpawner conf

--- dockerspawner/examples/swarm/jupyterhub_config.py
+++ dockerspawner/examples/swarm/jupyterhub_config.py
@@ -1,9 +1,9 @@
 import logging
+import os
 
 c = get_config()
 
 # The proxy is in another container
-c.DockerSpawner.use_internal_ip = True
 c.ConfigurableHTTPProxy.should_start = False
 c.ConfigurableHTTPProxy.api_url = 'http://proxy:8001'
 # tell the hub to use Dummy Auth (for testing)
@@ -13,34 +13,32 @@ c.JupyterHub.spawner_class = 'dockerspawner.SwarmSpawner'
 # The Hub should listen on all interfaces,
 # so user servers can connect
 # c.JupyterHub.hub_ip = '0.0.0.0'
-# this is the name of the 'service' in docker-compose.yml
-# c.JupyterHub.hub_connect_ip = 'jupytepide-hub'
-# The public facing port of the proxy
-c.JupyterHub.port = 8000
-# The ip for this process
-c.JupyterHub.hub_ip = 'jupytepide-hub'
-# this is the network name for jupyterhub in docker-compose.yml
-# with a leading 'swarm_' that docker-compose adds
+
+c.DockerSpawner.use_internal_ip = True
+
 c.SwarmSpawner.network_name = 'jupytepide-swarm-net'
 c.DockerSpawner.extra_host_config = {'network_mode': 'jupytepide-swarm-net'}
 c.DockerSpawner.extra_start_kwargs = {'network_mode': 'jupytepide-swarm-net'}
 
+c.JupyterHub.hub_ip = 'jupytepide-hub'
 c.DockerSpawner.hub_ip_connect = 'jupytepide-hub'
-# c.JupyterHub.hub_ip = '192.168.0.9'
 c.DockerSpawner.container_ip = "0.0.0.0"
+
+c.JupyterHub.port = 8000
 
 c.Spawner.start_timeout = 200
 c.Spawner.http_timeout = 200
 
 c.JupyterHub.cookie_secret_file = 'jupyterhub_cookie_secret'
 
-# start jupyterlab
-# c.Spawner.cmd = ["jupyter", "labhub"]
-
 # debug-logging for testing
 c.JupyterHub.log_level = logging.DEBUG
 
-c.DockerSpawner.image = 'jupyter/minimal-notebook:30f16d52126f'
+c.DockerSpawner.image = 'jupytepide/eodata-notebook:latest'
+
+notebook_dir = os.environ.get('DOCKER_NOTEBOOK_DIR') or '/home/jovyan/work'
+c.DockerSpawner.notebook_dir = notebook_dir
+
 
 # Explicitly set notebook directory because we'll be mounting a host volume to
 # it.  Most jupyter/docker-stacks *-notebook images run the Notebook server as
