36fefd0b49fb52f6cd0cbcf5299db505612ab796
zidan <daniel.zinkiewicz@wasat.pl>
2018-10-02 14:50:55 +0200

    Letsencryt added

--- swarm/build_letsencrypt.sh
+++ swarm/build_letsencrypt.sh
@@ -6,7 +6,9 @@ docker run \
   --cap-add=NET_ADMIN \
   --name nginx \
   -p 443:443 \
+  -p 80:80 \
   -e EMAIL=jupytep@wasat.pl \
   -e URL=jupyteo.com \
+  -e SUBDOMAINS=www \
   -v nginx_volume:/config \
   linuxserver/letsencrypt
