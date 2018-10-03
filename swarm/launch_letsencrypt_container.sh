62f2f277b20d3a1cca8f602dee3ec186a8847b5d
zidan <daniel.zinkiewicz@wasat.pl>
2018-10-03 11:07:40 +0200

    Clean commit

--- swarm/launch_letsencrypt_container.sh
+++ swarm/launch_letsencrypt_container.sh
@@ -6,7 +6,7 @@ docker run \
   -p 80:80 \
   --detach \
   -e EMAIL=jupytep@wasat.pl \
-  -e URL=jupyteo.com \
+  -e URL=jupytepide.ga \
   -v nginx_volume:/config \
   --network jupytepide-swarm-net \
   --mount type=bind,src=/home/eouser/jupytep-dev/swarm/letsencrypt_container_nginx.conf,dst=/config/nginx/site-confs/default \
