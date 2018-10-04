669eab70d0b0e6df1b80a07a938a4d0f59fe62b2
zidan <daniel.zinkiewicz@wasat.pl>
2018-10-04 14:40:47 +0200

    Added Geoserver, Postgis, Mapnik, PyWPS

--- spawn/build.sh
+++ spawn/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 docker build \
-    -t jupytepide/user-spawn-notebook:dev \
+    -t jupytepide/user-spawn-notebook:1.3.1 \
     --build-arg DOCKER_NOTEBOOK_IMAGE=jupytepide/eodata-notebook:1.3.0 \
     -f dockerfile .
