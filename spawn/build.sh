e4b09c1a4c54547c9afc1b71b98091e6dc5e6333
zidan <daniel.zinkiewicz@wasat.pl>
2018-11-09 23:57:15 +0100

    Add webiste with ssl conf

--- spawn/build.sh
+++ spawn/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 docker build \
-    -t jupytepide/user-spawn-notebook:test \
-    --build-arg DOCKER_NOTEBOOK_IMAGE=jupytepide/eodata-notebook:1.3.0 \
+    -t jupytepide/user-spawn-notebook:dev \
+    --build-arg DOCKER_NOTEBOOK_IMAGE=jupytepide/eodata-notebook:1.3.4 \
     -f Dockerfile .
--- swarm/rebuild_all.sh
+++ swarm/rebuild_all.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-docker service rm $(docker service ls -q)
-docker rm -f $(docker ps -a -q)
-docker-compose down
+# docker service rm $(docker service ls -q)
+# docker rm -f $(docker ps -a -q)
+# docker-compose down
 #docker rmi -f jupytepide-hub:latest
 #docker-compose build
 docker-compose up -d
