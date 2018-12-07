c3b7591f333d32db75e331911afef1b4277dd9c2
zidan <daniel.zinkiewicz@wasat.pl>
2018-12-07 15:24:10 +0100

    add sdk paths script

--- scripts/startup/05-sdk-paths.py
+++ scripts/startup/05-sdk-paths.py
@@ -1,12 +1,11 @@
-import os
 import sys
 
-
-def add_paths():
-    module_path1 = os.path.abspath(os.path.join('/opt/var/JupyTEPIDE/sdk'))
-    if module_path1 not in sys.path:
-        sys.path.append(module_path1)
-
-add_paths()
+sys.path.append("/opt/var/JupyTEPIDE/sdk")
+# def add_paths():
+#     module_path = os.path.abspath(os.path.join('/opt/var/JupyTEPIDE/sdk'))
+#     if module_path not in sys.path:
+#         sys.path.append(module_path)
+#
+# add_paths()
 
 print("Added script 05-sdk-paths.py")
