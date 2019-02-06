20b99c674be88239240b5a56b45bee621183483f
zidan <daniel.zinkiewicz@wasat.pl>
2019-02-06 10:04:00 +0100

    conf of registry

--- reg/make_reg_https.sh
+++ reg/make_reg_https.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
-docker run -d \
-  -p 443:443 \
+docker service create \
   --name registry \
-  -v /reg/registry/data:/var/lib/registry \
-  -v /reg/registry/security:/etc/security \
+  --mount type=bind,src=/reg/registry/data,dst=/var/lib/registry \
+  --mount type=bind,src=/reg/registry/security,dst=/etc/security \
   -e REGISTRY_HTTP_ADDR=0.0.0.0:443 \
   -e REGISTRY_HTTP_TLS_CERTIFICATE=/etc/security/fullchain1.pem \
   -e REGISTRY_HTTP_TLS_KEY=/etc/security/privkey1.pem \
   -e REGISTRY_AUTH=htpasswd \
   -e REGISTRY_AUTH_HTPASSWD_PATH=/etc/security/htpasswd \
   -e REGISTRY_AUTH_HTPASSWD_REALM="Registry Realm" \
-  --restart always \
+  --publish published=443,target=443 \
+  --constraint 'node.role==manager' \
+  --replicas 1 \
   registry:2
