5994ad79be11a8a73860217ada0dc3c4146579f5
Daniel Zinkiewicz <zinkiewicz.daniel@gamil.com>
2019-02-23 22:33:30 +0100

    Version 2.0.1

--- scripts/startup/01-otb-paths.py
+++ scripts/startup/01-otb-paths.py
@@ -1,12 +1,12 @@
 import os
 import sys
 
-sys.path.append("/opt/OTB-6.6.1-Linux64/lib/python")
-try:
-    user_paths = os.environ['PYTHONPATH'].split(os.pathsep)
-    print("JupyTEP IDE 01-otb-paths TEST")
-except KeyError:
-    user_paths = []
-os.environ['OTB_APPLICATION_PATH'] = "/opt/OTB-6.6.1-Linux64/lib/otb/applications"
+# sys.path.append("/opt/OTB-6.6.1-Linux64/lib/python")
+# try:
+#     user_paths = os.environ['PYTHONPATH'].split(os.pathsep)
+#     print("JupyTEP IDE 01-otb-paths TEST")
+# except KeyError:
+#     user_paths = []
+# os.environ['OTB_APPLICATION_PATH'] = "/opt/OTB-6.6.1-Linux64/lib/otb/applications"
 
 print("Added script 01-otb-paths.py")
