d96293ca338e4706a74a86d1e1d80a5264cb4bfa
Daniel Zinkiewicz <zinkiewicz.daniel@gamil.com>
2019-02-24 12:19:21 +0100

    Version 2.0.1

---
+++ docker/push.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+push_all()
+{
+	VER=$1
+	scipy ${VER}
+	geo ${VER}
+	eo ${VER}
+	sar ${VER}
+	deep ${VER}
+	r ${VER}
+	julia ${VER}
+	lab ${VER}
+	all ${VER}
+}
+
+scipy(){
+	VER=$1
+	IMAGE=reg.jupyteo.com/scipy-ext-notebook:${VER}
+	echo "START SEND TO ACTINA NODES: "${IMAGE}
+	push ${IMAGE}
+}
+
+geo(){
+	VER=$1
+	IMAGE=reg.jupyteo.com/geospatial-notebook:${VER}
+	echo "START SEND TO ACTINA NODES: "${IMAGE}
+	push ${IMAGE}
+}
+
+eo(){
+	VER=$1
+	IMAGE=reg.jupyteo.com/eodata-notebook:${VER}
+	echo "START SEND TO ACTINA NODES: "${IMAGE}
+	push ${IMAGE}
+}
+
+sar(){
+	VER=$1
+	IMAGE=reg.jupyteo.com/sar-notebook:${VER}
+	echo "START SEND TO ACTINA NODES: "${IMAGE}
+	push ${IMAGE}
+}
+
+deep(){
+	VER=$1
+	IMAGE=reg.jupyteo.com/deep-learning-notebook:${VER}
+	echo "START SEND TO ACTINA NODES: "${IMAGE}
+	push ${IMAGE}
+}
+
+r(){
+	VER=$1
+	IMAGE=reg.jupyteo.com/r-notebook:${VER}
+	echo "START SEND TO ACTINA NODES: "${IMAGE}
+	push ${IMAGE}
+}
+
+julia(){
+	VER=$1
+	IMAGE=reg.jupyteo.com/julia-notebook:${VER}
+	echo "START SEND TO ACTINA NODES: "${IMAGE}
+	push ${IMAGE}
+}
+
+lab(){
+	VER=$1
+	IMAGE=reg.jupyteo.com/data-lab-notebook:${VER}
+	echo "START SEND TO ACTINA NODES: "${IMAGE}
+	push ${IMAGE}
+}
+
+all(){
+	VER=$1
+	IMAGE=reg.jupyteo.com/all-in-one-notebook:${VER}
+	echo "START SEND TO ACTINA NODES: "${IMAGE}
+	push ${IMAGE}
+}
+
+push(){
+DOCKER_IMG=$1
+TIMEFORMAT='It takes %R seconds to complete push and pull'
+time {
+	echo "Push to Docker Registry: "${DOCKER_IMG}
+    docker push ${DOCKER_IMG}
+ }
+}
+
+"$@"
---
+++ docker/retag.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+
+tag_all()
+{
+	VER_1=$1
+	VER_2=$2
+	scipy ${VER_1} ${VER_2}
+	geo  ${VER_1} ${VER_2}
+	eo  ${VER_1} ${VER_2}
+	sar  ${VER_1} ${VER_2}
+	deep  ${VER_1} ${VER_2}
+	r  ${VER_1} ${VER_2}
+	julia  ${VER_1} ${VER_2}
+	lab  ${VER_1} ${VER_2}
+	all  ${VER_1} ${VER_2}
+}
+
+scipy(){
+	VER_1=$1
+	VER_2=$2
+	IMAGE_1=reg.jupyteo.com/scipy-ext-notebook:${VER_1}
+	IMAGE_2=reg.jupyteo.com/scipy-ext-notebook:${VER_2}
+	echo "START RETAG FORM: "${IMAGE_1} " TO: "${IMAGE_2}
+	tag ${IMAGE_1} ${IMAGE_2}
+}
+
+geo(){
+	VER_1=$1
+	VER_2=$2
+	IMAGE_1=reg.jupyteo.com/geospatial-notebook:${VER_1}
+	IMAGE_2=reg.jupyteo.com/geospatial-notebook:${VER_2}
+	echo "START RETAG FORM: "${IMAGE_1} " TO: "${IMAGE_2}
+	tag ${IMAGE_1} ${IMAGE_2}
+}
+
+eo(){
+	VER_1=$1
+	VER_2=$2
+	IMAGE_1=reg.jupyteo.com/eodata-notebook:${VER_1}
+	IMAGE_2=reg.jupyteo.com/eodata-notebook:${VER_2}
+	echo "START RETAG FORM: "${IMAGE_1} " TO: "${IMAGE_2}
+	tag ${IMAGE_1} ${IMAGE_2}
+}
+
+sar(){
+	VER_1=$1
+	VER_2=$2
+	IMAGE_1=reg.jupyteo.com/sar-notebook:${VER_1}
+	IMAGE_2=reg.jupyteo.com/sar-notebook:${VER_2}
+	echo "START RETAG FORM: "${IMAGE_1} " TO: "${IMAGE_2}
+	tag ${IMAGE_1} ${IMAGE_2}
+}
+
+deep(){
+	VER_1=$1
+	VER_2=$2
+	IMAGE_1=reg.jupyteo.com/deep-learning-notebook:${VER_1}
+	IMAGE_2=reg.jupyteo.com/deep-learning-notebook:${VER_2}
+	echo "START RETAG FORM: "${IMAGE_1} " TO: "${IMAGE_2}
+	tag ${IMAGE_1} ${IMAGE_2}
+}
+
+r(){
+	VER_1=$1
+	VER_2=$2
+	IMAGE_1=reg.jupyteo.com/r-notebook:${VER_1}
+	IMAGE_2=reg.jupyteo.com/r-notebook:${VER_2}
+	echo "START RETAG FORM: "${IMAGE_1} " TO: "${IMAGE_2}
+	tag ${IMAGE_1} ${IMAGE_2}
+}
+
+julia(){
+	VER_1=$1
+	VER_2=$2
+	IMAGE_1=reg.jupyteo.com/julia-notebook:${VER_1}
+	IMAGE_2=reg.jupyteo.com/julia-notebook:${VER_2}
+	echo "START RETAG FORM: "${IMAGE_1} " TO: "${IMAGE_2}
+	tag ${IMAGE_1} ${IMAGE_2}
+}
+
+lab(){
+	VER_1=$1
+	VER_2=$2
+	IMAGE_1=reg.jupyteo.com/data-lab-notebook:${VER_1}
+	IMAGE_2=reg.jupyteo.com/data-lab-notebook:${VER_2}
+	echo "START RETAG FORM: "${IMAGE_1} " TO: "${IMAGE_2}
+	tag ${IMAGE_1} ${IMAGE_2}
+}
+
+all(){
+	VER_1=$1
+	VER_2=$2
+	IMAGE_1=reg.jupyteo.com/all-in-one-notebook:${VER_1}
+	IMAGE_2=reg.jupyteo.com/all-in-one-notebook:${VER_2}
+	echo "START RETAG FORM: "${IMAGE_1} " TO: "${IMAGE_2}
+	tag ${IMAGE_1} ${IMAGE_2}
+}
+
+tag(){
+IMG_1=$1
+IMG_2=$2
+TIMEFORMAT='It takes %R seconds to complete'
+time {
+    docker tag ${IMG_1} ${IMG_2}
+ }
+}
+
+"$@"
